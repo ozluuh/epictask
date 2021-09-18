b057ea989985e9a6af9e5206b0aeb31d7e73f9d9
ozluuh <ozluuh@gmail.com>
2021-09-18 14:43:40 -0300

    build: add config to identify yaml file

---
+++ src/main/java/br/com/fiap/epictask/config/MessageSourceConfig.java
@@ -0,0 +1,64 @@
+package br.com.fiap.epictask.config;
+
+import java.util.Locale;
+import java.util.MissingResourceException;
+import java.util.ResourceBundle;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+import dev.akkinoc.util.YamlResourceBundle;
+
+@Configuration
+public class MessageSourceConfig {
+
+	@Bean
+	public MessageSource messageSource(
+			@Value("${spring.messages.basename}") String basename,
+			@Value("${spring.messages.encoding}") String encoding
+	) {
+		YamlMessageSource ms = new YamlMessageSource();
+		ms.setBasenames(basename);
+		ms.setDefaultEncoding(encoding);
+		ms.setAlwaysUseMessageFormat(true);
+		ms.setUseCodeAsDefaultMessage(true);
+		ms.setFallbackToSystemLocale(true);
+
+		return ms;
+	}
+
+	@Bean
+	public LocalValidatorFactoryBean validator(MessageSource messageSource) {
+		LocalValidatorFactoryBean bean = new LocalValidatorFactoryBean();
+		bean.setValidationMessageSource(messageSource);
+		return bean;
+	}
+
+	@Bean
+	public LocaleResolver localeResolver() {
+		SessionLocaleResolver localeResolver = new SessionLocaleResolver();
+		localeResolver.setDefaultLocale(Locale.US);
+		return localeResolver;
+	}
+
+	@Bean
+	public LocaleChangeInterceptor localeChangeInterceptor() {
+		LocaleChangeInterceptor localeChangeInterceptor = new LocaleChangeInterceptor();
+		localeChangeInterceptor.setParamName("lang");
+		return localeChangeInterceptor;
+	}
+}
+
+class YamlMessageSource extends ResourceBundleMessageSource {
+	@Override
+	protected ResourceBundle doGetBundle(String basename, Locale locale) throws MissingResourceException {
+		return ResourceBundle.getBundle(basename, locale, YamlResourceBundle.Control.INSTANCE);
+	}
+}
